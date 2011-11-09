9453e48893a006f1bdd69553a15c6befab0c7daa
dmackhack <dmackenz1981@gmail.com>
2011-11-09 20:36:01 +1100

    Moved everything into StatsService directory

--- src/test/java/org/dmack/statsapp/common/TestDataLoader.java
+++
@@ -1,8 +0,0 @@
-package org.dmack.statsapp.common;
-
-public interface TestDataLoader
-{
-    void load(String bundle, String test) throws DataLoadException;
-
-    void teardown(String bundle, String test) throws DataLoadException;
-}
